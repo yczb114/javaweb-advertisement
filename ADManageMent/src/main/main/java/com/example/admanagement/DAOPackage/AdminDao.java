cf58ee33f53c107434493714b3b3683b7091a312
Julius <162953648+13611897802@users.noreply.github.com>
2025-01-03 01:14:22 +0800

    Delete ADManageMent/src/main directory

--- ADManageMent/src/main/main/java/com/example/admanagement/DAOPackage/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.admanagement.DAOPackage;
-
-import java.util.List;
-
-public interface AdminDao extends DAO {
-    boolean checkAdmin(String adminName, int adminPassword);
-
-    List<InternetWebmaster> getAllWebmaster();
-}
